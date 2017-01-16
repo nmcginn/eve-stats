1cb80c91f8bea7eac09c7cfae4093d831a28436c
Nathan McGinn <nmcginn@users.noreply.github.com>
2017-01-15 21:20:50 -0600

    started rmd

--- top-deaded-ships.R
+++ top-deaded-ships.R
@@ -2,10 +2,12 @@ library(httr)
 library(jsonlite)
 library(dplyr)
 
+# get type ids
 ships <- "Sin|Panther|Redeemer|Widow"
 response <- GET(paste0("https://www.fuzzwork.co.uk/api/typeid2.php?typename=", ships))
 type_ids <- fromJSON(content(response, "text", encoding = "utf-8"))
 
+# zkill data pull
 dead_ships <- data.frame(row.names = FALSE)
 for (type_id in type_ids$typeID) {
   for (page in 1:1000) {
@@ -21,3 +23,11 @@ for (type_id in type_ids$typeID) {
     Sys.sleep(1) # look how nice I'm being
   }
 }
+
+# data shaping junk
+dead_ships$victim.shipTypeID <- as.character(dead_ships$victim.shipTypeID)
+dead_ships <- inner_join(dead_ships, type_ids, by = c("victim.shipTypeID" = "typeID"))
+save(dead_ships, file = "dead_ships.RData")
+
+#lol_pl <- dead_ships %>% filter(victim.allianceName == "Pandemic Legion")
+#waffe <- dead_ships %>% filter(victim.corporationName == "SniggWaffe")
