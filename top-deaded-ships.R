a0a8ee565de5796ae4161800e6e0e0941b3d5f17
Nathan McGinn <nmcginn@users.noreply.github.com>
2017-01-15 20:37:27 -0600

    that's better

--- top-deaded-ships.R
+++ top-deaded-ships.R
@@ -4,19 +4,20 @@ library(dplyr)
 
 ships <- "Sin|Panther|Redeemer|Widow"
 response <- GET(paste0("https://www.fuzzwork.co.uk/api/typeid2.php?typename=", ships))
-type_id <- fromJSON(content(response, "text", encoding = "utf-8"))
-type_ids <- paste(sort(type_id$typeID), collapse = ",")
+type_ids <- fromJSON(content(response, "text", encoding = "utf-8"))
 
 dead_ships <- data.frame(row.names = FALSE)
-for (page in 1:1000) {
-  top_losses_api <- paste0("https://zkillboard.com/api/losses/shipID/", type_ids, "/page/", page, "/")
-  response <- GET(top_losses_api, httr::add_headers(`Accept` = "gzip", `User-Agent` = "Derek Kanjus"))
-  killmails <- fromJSON(content(response, "text", encoding = "utf-8"), flatten = TRUE)
-  if (nrow(killmails) == 0) {
-    break()
-  }
-  killmails <- killmails %>% select(killID, killTime, victim.shipTypeID, victim.characterID, victim.characterName, victim.corporationName, victim.allianceName, zkb.totalValue)
+for (type_id in type_ids$typeID) {
+  for (page in 1:1000) {
+    top_losses_api <- paste0("https://zkillboard.com/api/losses/shipID/", type_id, "/page/", page, "/")
+    response <- GET(top_losses_api, httr::add_headers(`Accept` = "gzip", `User-Agent` = "Derek Kanjus"))
+    killmails <- fromJSON(content(response, "text", encoding = "utf-8"), flatten = TRUE)
+    if (length(killmails) == 0) {
+      break()
+    }
+    killmails <- killmails %>% select(killID, killTime, victim.shipTypeID, victim.characterID, victim.characterName, victim.corporationName, victim.allianceName, zkb.totalValue)
 
-  dead_ships <- rbind(dead_ships, killmails)
-  Sys.sleep(1) # look how nice I'm being
+    dead_ships <- rbind(dead_ships, killmails)
+    Sys.sleep(1) # look how nice I'm being
+  }
 }
